9fe508d13c6b06bdd4ec3e039eced6f09c28da2b
Asikson <Asikson>
2021-01-10 19:14:39 +0100

    Added Buy button reaction

--- server/routes/view.js
+++ server/routes/view.js
@@ -41,6 +41,16 @@ router.get('/buyProduct/:productId',  async (req, res) => {
     }
     res.render('buyProduct', {product});
 })
+
+router.get('/boughtProduct/:productId',  async (req, res) => {
+    let product;
+    try {
+        product = await Product.findById(req.params.productId);
+    } catch (error) {
+        res.json({message: error}).status(400);
+    }
+    console.log("You bought ", product.name, "!");
+})
     
 router.get('/contact',ensureAuthenticated,async (req,res)=> res.render('contact'))
 module.exports = router;
